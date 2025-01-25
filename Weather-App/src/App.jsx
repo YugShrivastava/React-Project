32503166b61480ae251f9f32c4b812ccbaa6840b
YugShrivastava <shrivastavayug04@gmail.com>
2025-01-25 20:50:14 +0530

    Completed Weather App

---
+++ Weather-App/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import WeatherCard from "./assets/components/WeatherCard";
+
+function App() {
+    return (
+    <>
+        <h1 className="text-3xl font-bold text-center py-14 text-stone-800">Weather Application</h1>
+        <WeatherCard />
+    </>
+    )
+}
+
+export default App;
